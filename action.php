d74e82e94a58b52dc012eec562cb4f7b9d076caa
Abhi241296 <abhishekhaksar35@gmail.com>
2019-10-11 15:49:32 -0700

    Update#6
    Now the Teacher can click on submit and the values are sent to the database

--- action.php
+++ action.php
@@ -1,5 +1,9 @@
 <?php
 print_r($_REQUEST['addmore']);
 print_r($_REQUEST['addmore1']);
+$Question_values_array = $_POST['addmore1'];
+print_r($Question_values_array);
+$Answer_values_array = $_POST['addmore'];
+print_r($Answer_values_array);
 exit;
 ?>
