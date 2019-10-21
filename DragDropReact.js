490bb194e12036b96ac227e5dba7ee7be3e424a9
ganatraharmish <54949296+ganatraharmish@users.noreply.github.com>
2019-10-20 23:43:13 -0700

    DropDown Added

--- DragDropReact.js
+++ DragDropReact.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 
 
+//Adding Dropdown
 
+handleClickOutside(){
+  this.setState({
+    listOpen: false
+  })
+}
+toggleList(){
+  this.setState(prevState => ({
+    listOpen: !prevState.listOpen
+  }))
+}
+render(){
+  const{list} = this.props
+  const{listOpen, headerTitle} = this.state
+  return(
+    <div className="dd-wrapper">
+<div className="dd-header" onClick={() => this.toggleList()}>
+        <div className="dd-header-title">{headerTitle}</div>
+        {listOpen
+          ? <FontAwesome name="angle-up" size="2x"/>
+          : <FontAwesome name="angle-down" size="2x"/>
+        }
+    </div>
+{listOpen && <ul className="dd-list">
+       {list.map((item) => (
+         <li className="dd-list-item" key={item.id} >{item.title}</li>
+        ))}
+      </ul>}
+    </div>
+  )
+}
 
 
 
