6202941cc31585d76b7ca474e6f6dd5b0b8cebe4
Sudhanva Hebbale <32284340+sudhanva-hebbale@users.noreply.github.com>
2019-11-19 10:03:34 -0700

    SQL File

--- Project.sql
+++ Project.sql
@@ -1,35 +1,35 @@
 CREATE TABLE Quiz (
-	Quiz_ID INT AUTO_INCREMENT, 
-	Name varchar(50) NOT NULL,
-	Email varchar(50) NOT NULL,
+	Name varchar(500),
+	Instructor varchar(100) NOT NULL,
+	Quiz_Level varchar(50) NOT NULL,
 	Description varchar(100),
-	PRIMARY KEY (Quiz_ID),
-	FOREIGN KEY (Email) REFERENCES Users(Email)
+	PRIMARY KEY (Name)
 );
 
 CREATE TABLE Questions (
-	Question_ID INT AUTO_INCREMENT,
+	ID INT AUTO_INCREMENT,
 	Question varchar(1000),
-	Quiz_ID INT,
-	PRIMARY KEY (Question_ID),
-	FOREIGN KEY (Quiz_ID) REFERENCES Quiz(Quiz_ID)
+	Quiz_Name varchar(500),
+	PRIMARY KEY (ID),
+	FOREIGN KEY (Quiz_Name) REFERENCES Quiz(Name)
 );
 
 CREATE TABLE Choices (
-	Choice_ID INT AUTO_INCREMENT,
+	ID INT AUTO_INCREMENT,
 	Choice varchar(100) NOT NULL,
 	IfAnswer varchar(1) NOT NULL,
-	Quiz_ID INT,
+	Quiz_Name varchar(500),
 	Question_ID INT,
-	PRIMARY KEY (Choice_ID),
-	FOREIGN KEY (Quiz_ID) REFERENCES Quiz(Quiz_ID),
-	FOREIGN KEY (Question_ID) REFERENCES Questions(Question_ID)
+	PRIMARY KEY (ID),
+	FOREIGN KEY (Quiz_Name) REFERENCES Quiz(Name),
+	FOREIGN KEY (Question_ID) REFERENCES Questions(ID)
 );
 
 CREATE TABLE Grades (
-	Quiz_ID INT NOT NULL,
+	Quiz_Name varchar(500),
 	Email varchar(50) NOT NULL,
 	Score varchar(3) NOT NULL,
-	FOREIGN KEY (Quiz_ID) REFERENCES Quiz(Quiz_ID),
+	Instructor varchar(50) NOT NULL,
+	FOREIGN KEY (Quiz_Name) REFERENCES Quiz(Name),
 	FOREIGN KEY (Email) REFERENCES Users(Email)
 );
